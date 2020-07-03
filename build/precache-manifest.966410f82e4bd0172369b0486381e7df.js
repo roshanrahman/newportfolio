01dc6851cb9b7d6093b749a2d706abb8aeda4d91
Roshan <roshanrahman6399@gmail.com>
2020-07-03 21:59:36 +0530

    Build

---
+++ build/precache-manifest.966410f82e4bd0172369b0486381e7df.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86431111267b8205b9ee5a3a5d0da87a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8efd7096e53739d7e515",
+    "url": "/static/css/main.ec9480fc.chunk.css"
+  },
+  {
+    "revision": "bae5d1972781dd2200f5",
+    "url": "/static/js/2.760d64c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.760d64c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8efd7096e53739d7e515",
+    "url": "/static/js/main.2f76a2dc.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "f8bb3debac94958e4e180e3ea6153d9c",
+    "url": "/static/media/email-dark.f8bb3deb.svg"
+  },
+  {
+    "revision": "b585abb1cf27d373daa07ad6e4cf6d37",
+    "url": "/static/media/email-mob-dark.b585abb1.svg"
+  },
+  {
+    "revision": "abb57146a78b4b6a02d80f411e61988c",
+    "url": "/static/media/email-mob.abb57146.svg"
+  },
+  {
+    "revision": "b733324a32e7af9b587db26a98896e1a",
+    "url": "/static/media/email.b733324a.svg"
+  }
+]);
