64c545b0a0bb65226015abcef243ea66c24614be
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-03 17:10:20 +0000

    Deploy roshanrahman/newportfolio to github.com/roshanrahman/newportfolio.git:gh-pages

---
+++ precache-manifest.b8b5bd8d3c470267a45f8103d970f8b1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ee29464b42606f66ea0769abcae8bd3",
+    "url": "/newportfolio/index.html"
+  },
+  {
+    "revision": "eff83d876ebb12a7533d",
+    "url": "/newportfolio/static/css/main.ec9480fc.chunk.css"
+  },
+  {
+    "revision": "bae5d1972781dd2200f5",
+    "url": "/newportfolio/static/js/2.760d64c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/newportfolio/static/js/2.760d64c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eff83d876ebb12a7533d",
+    "url": "/newportfolio/static/js/main.11b3cedf.chunk.js"
+  },
+  {
+    "revision": "58d83d9ed1bd5b0f16db",
+    "url": "/newportfolio/static/js/runtime-main.6870a5fc.js"
+  },
+  {
+    "revision": "f8bb3debac94958e4e180e3ea6153d9c",
+    "url": "/newportfolio/static/media/email-dark.f8bb3deb.svg"
+  },
+  {
+    "revision": "b585abb1cf27d373daa07ad6e4cf6d37",
+    "url": "/newportfolio/static/media/email-mob-dark.b585abb1.svg"
+  },
+  {
+    "revision": "abb57146a78b4b6a02d80f411e61988c",
+    "url": "/newportfolio/static/media/email-mob.abb57146.svg"
+  },
+  {
+    "revision": "b733324a32e7af9b587db26a98896e1a",
+    "url": "/newportfolio/static/media/email.b733324a.svg"
+  }
+]);
