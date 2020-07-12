c94adf2b03452e0693d1df6731ff32685fe2c8c8
Roshan <roshanrahman6399@gmail.com>
2020-07-12 22:40:24 +0530

    Updated content

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 
 # Copying assets/master.json to src/assets/master.json
 cat assets/master.json >src/assets/master.json
+git commit -m "updated src/assets/master.json"
 git push origin master
 git push heroku master
